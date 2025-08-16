7d2503d21a8b0b78e4498fac622dbe5f95dbb2b2
basesnel <basesnel@gmail.com>
2025-08-16 10:05:58 +0300

    Added Decor-element for cards.

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -19,56 +19,7 @@ const Card = ({ image, amount }) => {
           className={`${styles.card} ${styles.back}`}
           onClick={() => setIsOpen(false)}
         >
-          {image.label === "cash" && (
-            <>
-              <div className={styles.cashFirstBG} />
-              <div className={styles.cashLight} />
-              <div className={styles.cashGreenTop} />
-              <div className={styles.cashYellow} />
-              <div className={styles.cashGreenBottom} />
-              <div className={styles.cashSecondBG} />
-              <div className={styles.cashThirdBG} />
-            </>
-          )}
-          {image.label === "double" && (
-            <>
-              <div className={styles.cashFirstBG} />
-              <div className={styles.doubleLight} />
-              <div className={styles.doubleBlue} />
-              <div className={styles.doubleWhite} />
-              <div className={styles.doubleKonfetti} />
-              <div className={styles.doubleSecondWhite} />
-              <div className={styles.doubleSecondBG} />
-              <div className={styles.cashThirdBG} />
-            </>
-          )}
-          {image.label === "zero" && (
-            <>
-              <div className={styles.cashFirstBG} />
-              <div className={styles.zeroLight} />
-              <div className={styles.zeroYellowTop} />
-              <div className={styles.zeroYellowBottom} />
-              <div className={styles.zeroSecondBG} />
-              <div className={styles.cashThirdBG} />
-            </>
-          )}
-          {image.label === "stop" && (
-            <>
-              <div className={styles.stopLight} />
-              <div className={styles.stopBG} />
-            </>
-          )}
-          {image.label === "bomb" && (
-            <>
-              <div className={styles.bombFirstBG} />
-              <div className={styles.bombLight} />
-              <div className={styles.bombYellow} />
-              <div className={styles.bombRed} />
-              <div className={styles.bombRedSecond} />
-              <div className={styles.bombWhite} />
-              <div className={styles.cashThirdBG} />
-            </>
-          )}
+          <Decor label={image.label} />
           <div className={styles[`icon-${image.label}`]}>
             <Image image={image} />
             {amount && (
@@ -79,6 +30,70 @@ const Card = ({ image, amount }) => {
       )}
     </>
   );
+};
+
+const Decor = ({ label }) => {
+  switch (label) {
+    case "cash":
+      return (
+        <>
+          <div className={styles.cashFirstBG} />
+          <div className={styles.cashLight} />
+          <div className={styles.cashGreenTop} />
+          <div className={styles.cashYellow} />
+          <div className={styles.cashGreenBottom} />
+          <div className={styles.cashSecondBG} />
+          <div className={styles.cashThirdBG} />
+        </>
+      );
+
+    case "double":
+      return (
+        <>
+          <div className={styles.cashFirstBG} />
+          <div className={styles.doubleLight} />
+          <div className={styles.doubleBlue} />
+          <div className={styles.doubleWhite} />
+          <div className={styles.doubleKonfetti} />
+          <div className={styles.doubleSecondWhite} />
+          <div className={styles.doubleSecondBG} />
+          <div className={styles.cashThirdBG} />
+        </>
+      );
+
+    case "zero":
+      return (
+        <>
+          <div className={styles.cashFirstBG} />
+          <div className={styles.zeroLight} />
+          <div className={styles.zeroYellowTop} />
+          <div className={styles.zeroYellowBottom} />
+          <div className={styles.zeroSecondBG} />
+          <div className={styles.cashThirdBG} />
+        </>
+      );
+
+    case "stop":
+      return (
+        <>
+          <div className={styles.stopLight} />
+          <div className={styles.stopBG} />
+        </>
+      );
+
+    case "bomb":
+      return (
+        <>
+          <div className={styles.bombFirstBG} />
+          <div className={styles.bombLight} />
+          <div className={styles.bombYellow} />
+          <div className={styles.bombRed} />
+          <div className={styles.bombRedSecond} />
+          <div className={styles.bombWhite} />
+          <div className={styles.cashThirdBG} />
+        </>
+      );
+  }
 };
 
 export default Card;
