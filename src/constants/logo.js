1d50bfbb76555d900fce30a27a2963e4dc84cf1b
basesnel <basesnel@gmail.com>
2025-08-25 18:54:55 +0300

    Added retina to Logo-image.

--- src/constants/logo.js
+++ src/constants/logo.js
@@ -1,9 +1,11 @@
-import LogoWBP from "../assets/logo/logo.webp";
-import LogoPNG from "../assets/logo/logo.png";
+import LogoWBP from "../assets/logo/logo-1x.webp";
+import LogoPNG from "../assets/logo/logo-1x.png";
+import Logo2xPNG from "../assets/logo/logo-2x.png";
 
 const logo = {
   wbp: LogoWBP,
   png: LogoPNG,
+  png2x: Logo2xPNG,
   width: 62,
   height: 35,
   label: "Star Industry logo",
