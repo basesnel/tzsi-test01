848e2ace686c8c3bb8dd6d6d5261ddf98c3f9a48
basesnel <basesnel@gmail.com>
2025-08-25 18:57:12 +0300

    Added retina to Logo-image.

--- src/constants/logo.js
+++ src/constants/logo.js
@@ -1,10 +1,12 @@
 import LogoWBP from "../assets/logo/logo-1x.webp";
 import LogoPNG from "../assets/logo/logo-1x.png";
+import Logo2xWBP from "../assets/logo/logo-2x.webp";
 import Logo2xPNG from "../assets/logo/logo-2x.png";
 
 const logo = {
   wbp: LogoWBP,
   png: LogoPNG,
+  wbp2x: Logo2xWBP,
   png2x: Logo2xPNG,
   width: 62,
   height: 35,
