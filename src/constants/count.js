6de177130623ab85d32bec20f6cd520952521adb
basesnel <basesnel@gmail.com>
2025-08-26 11:18:25 +0300

    Added retina to counter.

--- src/constants/count.js
+++ src/constants/count.js
@@ -2,6 +2,7 @@ import countWBP from "../assets/counter/count-1x.webp";
 import countPNG from "../assets/counter/count-1x.png";
 import count2xWBP from "../assets/counter/count-2x.webp";
 import count2xPNG from "../assets/counter/count-2x.png";
+import count3xWBP from "../assets/counter/count-3x.webp";
 import count3xPNG from "../assets/counter/count-3x.png";
 
 const count = {
@@ -10,6 +11,7 @@ const count = {
   png: countPNG,
   wbp2x: count2xWBP,
   png2x: count2xPNG,
+  wbp3x: count3xWBP,
   png3x: count3xPNG,
   width: 40,
   height: 40,
