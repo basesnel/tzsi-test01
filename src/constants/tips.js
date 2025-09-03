c2155d05b0815976d4beb24174ae9a85f3eb401e
basesnel <basesnel@gmail.com>
2025-09-03 10:16:53 +0300

    Added retina to cash-tip.

--- src/constants/tips.js
+++ src/constants/tips.js
@@ -3,6 +3,7 @@ import cash1xPNG from "../assets/tips/01-cash-1x.png";
 import cash2xWBP from "../assets/tips/01-cash-2x.webp";
 import cash2xPNG from "../assets/tips/01-cash-2x.png";
 import cash3xWBP from "../assets/tips/01-cash-3x.webp";
+import cash3xPNG from "../assets/tips/01-cash-3x.png";
 import x2WBP from "../assets/tips/02x2.webp";
 import x2PNG from "../assets/tips/02x2.png";
 import zeroWBP from "../assets/tips/03zero.webp";
@@ -23,6 +24,7 @@ const tips = [
       wbp2x: cash2xWBP,
       png2x: cash2xPNG,
       wbp3x: cash3xWBP,
+      png3x: cash3xPNG,
       width: 32,
       height: 32,
     },
