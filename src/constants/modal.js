a310a11f535adc8b8d8a67540b22cee4684ba023
basesnel <basesnel@gmail.com>
2025-09-08 11:31:53 +0300

    Added retina to buttonDefuse-modal.

--- src/constants/modal.js
+++ src/constants/modal.js
@@ -24,6 +24,7 @@ import buttonBomb3xWBP from "../assets/modal/04-bomb-3x.webp";
 import buttonBomb3xPNG from "../assets/modal/04-bomb-3x.png";
 import buttonDefuseWBP from "../assets/modal/05-defuse-1x.webp";
 import buttonDefuse1xPNG from "../assets/modal/05-defuse-1x.png";
+import buttonDefuse2xWBP from "../assets/modal/05-defuse-2x.webp";
 import buttonDefuse2xPNG from "../assets/modal/05-defuse-2x.png";
 
 const bomb = {
@@ -78,6 +79,7 @@ const buttonDefuse = {
   label: "Defuse",
   wbp: buttonDefuseWBP,
   png: buttonDefuse1xPNG,
+  wbp2x: buttonDefuse2xWBP,
   png2x: buttonDefuse2xPNG,
   width: 24,
   height: 24,
