6ca95925f9dfdf9944941d992de9bb0d54a7d20f
basesnel <basesnel@gmail.com>
2025-09-17 08:13:45 +0300

    Added Bomb and Stop components.

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -30,87 +30,85 @@ const Modal = ({ showModal, setShowModal, gameState, count }) => {
               onClick={(e) => e.stopPropagation()}
             >
               <Navbar />
-              {gameState === "bomb" && (
-                <>
-                  <div className={styles.light}>
-                    <div className={styles.bombRed} />
-                    <div className={styles.bombWhite} />
-                    <Image image={bomb} />
-                  </div>
-                  <section className={styles.content}>
-                    <h2 className={styles.title}>Danger ahead!</h2>
-                    <p className={`${styles.text} ${styles.firstText}`}>
-                      You're on a Bomb Square! You hit a bomb and lose all
-                      rewards from this field...
-                    </p>
-                    <figure className={styles.figure}>
-                      <Image image={cash} />
-                      <figcaption className={styles.caption}>
-                        {count}
-                      </figcaption>
-                    </figure>
-                    <p className={`${styles.text} ${styles.secondText}`}>
-                      ...or defuse it and save your run!
-                    </p>
-                    <ul className={styles.control}>
-                      <li className={styles.item}>
-                        <button className={styles.button}>
-                          <Image image={buttonBomb} />
-                          Take a hit
-                        </button>
-                      </li>
-                      <li className={styles.item}>
-                        <button
-                          className={`${styles.button} ${styles.secondButton}`}
-                        >
-                          Defuse for
-                          <Image image={buttonDefuse} />
-                          49
-                        </button>
-                      </li>
-                    </ul>
-                  </section>
-                </>
-              )}
-              {gameState === "stop" && (
-                <>
-                  <div className={styles.stopLight}>
-                    <Image image={stop} />
-                  </div>
-                  <section className={styles.content}>
-                    <h2 className={`${styles.title} ${styles.titleOver}`}>
-                      Game over!
-                    </h2>
-                    <p className={`${styles.text} ${styles.firstText}`}>
-                      You've reached the end of this run...
-                    </p>
-                    <figure className={styles.figure}>
-                      <Image image={cash} />
-                      <figcaption className={styles.caption}>
-                        {count}
-                      </figcaption>
-                    </figure>
-                    <p className={`${styles.text} ${styles.secondText}`}>
-                      ...claim and return to the main board
-                    </p>
-                    <ul className={styles.control}>
-                      <li className={styles.item}>
-                        <button
-                          className={`${styles.button} ${styles.thirdButton}`}
-                        >
-                          Claim
-                        </button>
-                      </li>
-                    </ul>
-                  </section>
-                </>
-              )}
+              {gameState === "bomb" && <Bomb count={count} />}
+              {gameState === "stop" && <Stop count={count} />}
             </div>
           </div>
         </div>
       )}
     </>,
     document.body
+  );
+};
+
+const Bomb = ({ count }) => {
+  return (
+    <>
+      <div className={styles.light}>
+        <div className={styles.bombRed} />
+        <div className={styles.bombWhite} />
+        <Image image={bomb} />
+      </div>
+      <section className={styles.content}>
+        <h2 className={styles.title}>Danger ahead!</h2>
+        <p className={`${styles.text} ${styles.firstText}`}>
+          You're on a Bomb Square! You hit a bomb and lose all rewards from this
+          field...
+        </p>
+        <figure className={styles.figure}>
+          <Image image={cash} />
+          <figcaption className={styles.caption}>{count}</figcaption>
+        </figure>
+        <p className={`${styles.text} ${styles.secondText}`}>
+          ...or defuse it and save your run!
+        </p>
+        <ul className={styles.control}>
+          <li className={styles.item}>
+            <button className={styles.button}>
+              <Image image={buttonBomb} />
+              Take a hit
+            </button>
+          </li>
+          <li className={styles.item}>
+            <button className={`${styles.button} ${styles.secondButton}`}>
+              Defuse for
+              <Image image={buttonDefuse} />
+              49
+            </button>
+          </li>
+        </ul>
+      </section>
+    </>
+  );
+};
+
+const Stop = ({ count }) => {
+  return (
+    <>
+      <div className={styles.stopLight}>
+        <Image image={stop} />
+      </div>
+      <section className={styles.content}>
+        <h2 className={`${styles.title} ${styles.titleOver}`}>Game over!</h2>
+        <p className={`${styles.text} ${styles.firstText}`}>
+          You've reached the end of this run...
+        </p>
+        <figure className={styles.figure}>
+          <Image image={cash} />
+          <figcaption className={styles.caption}>{count}</figcaption>
+        </figure>
+        <p className={`${styles.text} ${styles.secondText}`}>
+          ...claim and return to the main board
+        </p>
+        <ul className={styles.control}>
+          <li className={styles.item}>
+            <button className={`${styles.button} ${styles.thirdButton}`}>
+              Claim
+            </button>
+          </li>
+        </ul>
+      </section>
+    </>
   );
 };
 
