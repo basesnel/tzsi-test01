c743d4b50968aee1decf542639ecbf705a196385
basesnel <basesnel@gmail.com>
2026-05-13 13:36:51 +0300

    Updated styles of game-front.

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -16,9 +16,6 @@ const Game = () => {
 
   return (
     <div className={styles.game}>
-      <div className={styles.purple} />
-      <div className={styles.purpleLight} />
-      <div className={styles.stars} />
       <Navbar />
       <Title title="roll craft" />
       <Counter counter={count} />
